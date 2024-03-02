e53c39d4a931130ed3d5e591bb6c5e674deeaebf
Shaijijohnson <141117310+Shaijijohnson@users.noreply.github.com>
2024-03-02 16:41:44 +0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+site='learnpython,ai and datascience'
+if site == 'learnpython,ai and datascience':
+    print('Logging to Learning Site...')
+else:
+    print('Retype the URL')
+print('Ready To Learn!')
