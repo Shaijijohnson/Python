39ed04254a4050941ac9dca21dcb408de73caec9
Shaijijohnson <141117310+Shaijijohnson@users.noreply.github.com>
2024-03-04 21:25:13 +0400

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+fruits=["apple","banana","cherry","orange"]
+print(fruits)
+#next code
+fruits=["apple","banana","cherry","orange"]
+print(len(fruits))
+#next code 
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
